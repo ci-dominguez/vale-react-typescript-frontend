5a480f7aeadfc366553430c0133cd211f4735ee4
Cristian <cdom127@wgu.edu>
2024-12-10 22:23:01 -0800

    Update getDaysInMonth function to include the weekday as well. Start the table layout.

--- src/components/TrackerGrid.tsx
+++ src/components/TrackerGrid.tsx
@@ -1,41 +1,110 @@
 import { useState } from 'react';
 import DatePicker from './DatePicker';
 
-const getDaysInMonth = (year: number, month: number): number => {
-  const date = new Date(year, month + 1, 0);
-  return date.getDate();
+const getDaysInMonth = (year: number, month: number) => {
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+
+  return Array.from({ length: daysInMonth }, (_, i) => {
+    const date = new Date(year, month, i + 1);
+    return {
+      dayNum: i + 1,
+      dayOfTheWeek: weekdays[date.getDay()],
+    };
+  });
 };
 
 const TrackerGrid = () => {
-  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
-  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
-  const daysArray = Array.from(
-    { length: getDaysInMonth(currentYear, currentMonth) },
-    (_, i) => i + 1
-  );
+  const currentDate = {
+    day: new Date().getDate(),
+    month: new Date().getMonth() + 1,
+    year: new Date().getFullYear(),
+  };
+  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth());
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const daysArray = getDaysInMonth(selectedYear, selectedMonth);
 
   return (
-    <div className='flex flex-col items-center bg-orange-300'>
+    <div className='flex flex-col items-center'>
       <DatePicker
-        currentMonth={currentMonth}
-        currentYear={currentYear}
-        setCurrentMonth={setCurrentMonth}
-        setCurrentYear={setCurrentYear}
+        selectedMonth={selectedMonth}
+        selectedYear={selectedYear}
+        setSelectedMonth={setSelectedMonth}
+        setSelectedYear={setSelectedYear}
       />
-      <div className='flex bg-yellow-300'>
-        <div>Habits</div>
-        <div className='flex flex-row gap-1 bg-lime-300'>
-          {daysArray.map((day) => {
-            return (
-              <div
-                key={day}
-                className='border-[1px] border-gray-400 bg-gray-200 px-1'
+      <div className='overflow-x-none overflow-y-hidden w-full'>
+        <table className=' min-w-full table-fixed border-collapse border'>
+          <colgroup>
+            <col className='w-20' />
+            {daysArray.map((_, index) => (
+              <col key={`col-${index}`} className='w-4' />
+            ))}
+            <col className='w-12' />
+            <col className='w-8' />
+          </colgroup>
+
+          <thead>
+            <tr>
+              <th
+                className='sticky border text-center align-middle whitespace-nowrap'
+                rowSpan={2}
               >
-                {day}
-              </div>
-            );
-          })}
-        </div>
+                Habits
+              </th>
+
+              {daysArray.map((day) => (
+                <th
+                  key={`weekday-${day.dayNum}`}
+                  className={`border text-center align-middle whitespace-nowrap ${
+                    day.dayNum === currentDate.day &&
+                    selectedMonth === currentDate.month - 1 &&
+                    selectedYear === currentDate.year &&
+                    'bg-violet-300'
+                  }`}
+                >
+                  {day.dayOfTheWeek}
+                </th>
+              ))}
+              <th
+                className='border text-center align-middle whitespace-nowrap'
+                rowSpan={2}
+              >
+                Goal
+              </th>
+              <th
+                className='border text-center align-middle whitespace-nowrap'
+                rowSpan={2}
+              >
+                Achieved
+              </th>
+            </tr>
+
+            <tr>
+              {daysArray.map((day) => (
+                <th
+                  key={`day-${day.dayNum}`}
+                  className={`border text-center align-middle whitespace-nowrap py-5 ${
+                    day.dayNum === currentDate.day &&
+                    selectedMonth === currentDate.month - 1 &&
+                    selectedYear === currentDate.year &&
+                    'bg-violet-300'
+                  }`}
+                >
+                  {day.dayNum}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td colSpan={daysArray.length + 3} className='border'>
+                <button className='bg-slate-100 py-4 w-full text-center font-semibold'>
+                  Click here to get started and create a new habit!
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
