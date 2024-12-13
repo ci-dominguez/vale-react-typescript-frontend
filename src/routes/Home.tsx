41129ed31bd7f9dd3d8861e9d4e6df86531ee5b7
Cristian <cdom127@wgu.edu>
2024-12-12 16:32:35 -0800

    Add metadata to HomePage

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,16 @@
 import MainLayout from '../layouts/MainLayout';
 
 const HomePage = () => {
-  return <MainLayout>Hello Home!</MainLayout>;
+  return (
+    <>
+      <title>Vale | Habit Tracker</title>
+      <meta
+        name='description'
+        content='Welcome to Vale. Our app helps you build and maintain positive habits with ease. Track your habits monthly, set goals, and manage your progress seamlessly. Join us today and start building the habits that lead to success!'
+      />
+      <MainLayout>Hello Home!</MainLayout>
+    </>
+  );
 };
 
 export default HomePage;
