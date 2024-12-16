ad8fce0e882ef9e5826325e49374411fa7d532c6
Cristian <cdom127@wgu.edu>
2024-12-15 16:08:43 -0800

    Configure custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        editorial: ['editorial'],
+        'editorial-italic': ['editorial-italic'],
+        montreal: ['montreal'],
+      },
+    },
   },
   plugins: [],
 };
