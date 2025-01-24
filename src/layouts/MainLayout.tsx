0b2e60008cca8da0a7a7f809afc2e201f60c17ed
Cristian <cdom127@wgu.edu>
2025-01-24 13:41:23 -0800

    Add footer to layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,3 +1,4 @@
+import Footer from '../components/navigation/Footer';
 import Nav from '../components/navigation/Nav';
 
 interface Props {
@@ -11,6 +12,7 @@ const MainLayout = ({ children }: Props) => {
       <main className='min-h-screen bg-white text-charcoal px-4 md:px-6 lg:px-10 xl:px-20 3xl:px-52'>
         {children}
       </main>
+      <Footer />
     </>
   );
 };
